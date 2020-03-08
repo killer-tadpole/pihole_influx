b305c94b10e2512c92133a8372298ff9034a8006
Colin McGill <mcgill.cs@gmail.com>
2020-03-08 09:33:06 +0000

    Added venv path and requirements. Added check for DNS before start & reduced delay

--- pihole_influx.py
+++ pihole_influx.py
@@ -8,46 +8,48 @@
 
 import requests
 import time
+import uptime
 from influxdb import InfluxDBClient
 
-HOSTNAME = "ns-01" # Pi-hole hostname to report in InfluxDB for each measurement
-PIHOLE_API = "http://PI_HOLE_IP_ADDRESS_HERE/admin/api.php"
-INFLUXDB_SERVER = "127.0.0.1" # IP or hostname to InfluxDB server
+HOSTNAME = "home" # Pi-hole hostname to report in InfluxDB for each measurement
+PIHOLE_API = "http://pihole.home/admin/api.php"
+INFLUXDB_SERVER = "rpi3" # IP or hostname to InfluxDB server
 INFLUXDB_PORT = 8086 # Port on InfluxDB server
-INFLUXDB_USERNAME = "username"
+INFLUXDB_USERNAME = "root"
 INFLUXDB_PASSWORD = "password"
-INFLUXDB_DATABASE = "piholestats"
-DELAY = 600 # seconds
+INFLUXDB_DATABASE = "home"
+DELAY = 60 # seconds
+
 
 def send_msg(domains_being_blocked, dns_queries_today, ads_percentage_today, ads_blocked_today):
+   json_body = [
+       {
+           "measurement": "pihole." + HOSTNAME.replace(".", "_"),
+           "tags": {
+              "host": HOSTNAME
+           },
+           "fields": {
+              "domains_being_blocked": int(domains_being_blocked),
+              "dns_queries_today": int(dns_queries_today),
+              "ads_percentage_today": float(ads_percentage_today),
+              "ads_blocked_today": int(ads_blocked_today),
+              "uptime": int(uptime.uptime())
+           }
+       }
+    ]
+   print(json_body)
+   # InfluxDB host, InfluxDB port, Username, Password, database
+   client = InfluxDBClient(INFLUXDB_SERVER, INFLUXDB_PORT, INFLUXDB_USERNAME, INFLUXDB_PASSWORD, INFLUXDB_DATABASE) 
 
-	json_body = [
-	    {
-	        "measurement": "piholestats." + HOSTNAME.replace(".", "_"),
-	        "tags": {
-	            "host": HOSTNAME
-	        },
-	        "fields": {
-	            "domains_being_blocked": int(domains_being_blocked),
-                    "dns_queries_today": int(dns_queries_today),
-                    "ads_percentage_today": float(ads_percentage_today),
-                    "ads_blocked_today": int(ads_blocked_today)
-	        }
-	    }
-	]
-        print (json_body)
-        # InfluxDB host, InfluxDB port, Username, Password, database
-	client = InfluxDBClient(INFLUXDB_SERVER, INFLUXDB_PORT, INFLUXDB_USERNAME, INFLUXDB_PASSWORD, INFLUXDB_DATABASE) 
+   # Uncomment to create the database (expected to exist prior to feeding it data)
+   # client.create_database(INFLUXDB_DATABASE) 
 
-        # Uncomment to create the database (expected to exist prior to feeding it data)
-	# client.create_database(INFLUXDB_DATABASE) 
-
-	client.write_points(json_body)
-        print(json_body)
+   client.write_points(json_body)
+   print(json_body)
 
 if __name__ == '__main__':
         while True:
-          time.sleep(DELAY)
+          #time.sleep(DELAY)
           api = requests.get(PIHOLE_API) # URI to pihole server api
           API_out = api.json()
           domains_being_blocked = (API_out['domains_being_blocked'])
@@ -57,3 +59,4 @@ if __name__ == '__main__':
           # print(domains_being_blocked, dns_queries_today, ads_percentage_today, ads_blocked_today)
 
           send_msg(domains_being_blocked, dns_queries_today, ads_percentage_today, ads_blocked_today)
+          time.sleep(DELAY)
